2c3e4da5da6b8280950921f7b30b034efbcafcc7
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-26 19:07:27 -0800

    update navbar width

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,24 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/code-braces.svg";
-import User from "@/public/user-smile.svg";
-import Searchbar from "./Searchbar";
 import Button from "./ButtonLogin";
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <Link
-          href={"/"}
-          className="btn btn-ghost text-xl font-extrabold text-neutral"
-        >
-          <Image src={Logo} alt="Logo" className="h-10 w-10 text-base-200" />
-          <span className="hidden md:block">Developers Roadmap</span>
-        </Link>
-      </div>
-      <div className="flex gap-2">
-        <Button />
+    <div className="max-w-5xl mx-auto bg-base-100">
+      <div className="navbar   ">
+        <div className="flex-1">
+          <Link
+            href={"/"}
+            className="btn btn-ghost text-xl font-extrabold text-neutral"
+          >
+            <Image src={Logo} alt="Logo" className="h-10 w-10 text-base-200" />
+            <span className="hidden md:block">Developers Roadmap</span>
+          </Link>
+        </div>
+        <div className="flex gap-2">
+          <Button />
+        </div>
       </div>
     </div>
   );
