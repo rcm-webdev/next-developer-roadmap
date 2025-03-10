70becdc2731fb5fce9a77616ca96a204afd8e140
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-10 13:12:25 -0700

    add Hero, Footer, and Nav components

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
-import Counter from "@/component/Counter";
+import Footer from "@/component/Footer";
+import Hero from "@/component/Hero";
+import Navbar from "@/component/Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="bold decoration-amber-300 underline underline-offset-4 text-4xl">
-          Hello World
-        </h1>
-        <Counter title="Button" />
+    <div className="">
+      <main>
+        <Navbar />
+        <Hero />
+        <Footer />
       </main>
     </div>
   );
