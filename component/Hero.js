69c6672ebf8968e160992375b620cf4413bcc3c5
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-10 13:59:59 -0700

    add track pprogress btn

--- component/Hero.js
+++ component/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image src={Zombie} alt="Zombie" className="w-3/4" />
+        <Image src={Zombie} alt="Zombie" className="hidden md:block w-3/4" />
         <div>
           <div className="flex gap-4 items-center">
             <h1 className="text-5xl font-bold text-neutral">
@@ -15,11 +15,17 @@ function Hero() {
             <Image src={Roadmap} alt="Roadmap" className="w-14 h-14" />
           </div>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Follow the 100Devs roadmap set out for you to go from zero to
+            employed. There is a lot of work ahead, but trust the process.
+            Remember that you owe you. <br />
+            <span className="badge badge-accent font-semibold">
+              We are community taught.
+            </span>
           </p>
-          <button className="btn btn-primary">Get Roadmap</button>
+          <div className="flex gap-2 justify-center md:justify-start">
+            <button className="btn btn-primary">Get Roadmap</button>
+            <button className="btn btn-accent">Track Progress</button>
+          </div>
         </div>
       </div>
     </div>
