4a1da66b745e727531fd2326b51c951d38c7b554
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-10 14:18:45 -0700

    update hero section

--- component/Hero.js
+++ component/Hero.js
@@ -14,14 +14,16 @@ function Hero() {
             </h1>
             <Image src={Roadmap} alt="Roadmap" className="w-14 h-14" />
           </div>
-          <p className="py-6">
-            Follow the 100Devs roadmap set out for you to go from zero to
-            employed. There is a lot of work ahead, but trust the process.
-            Remember that you owe you. <br />
-            <span className="badge badge-accent font-semibold">
+          <div className="flex flex-col gap-2 py-6">
+            <p>
+              Follow the 100Devs roadmap set out for you to go from zero to
+              employed. There is a lot of work ahead, but trust the process.
+              Remember that you owe you.
+            </p>
+            <p className="badge badge-accent font-semibold ">
               We are community taught.
-            </span>
-          </p>
+            </p>
+          </div>
           <div className="flex gap-2 justify-center md:justify-start">
             <button className="btn btn-primary">Get Roadmap</button>
             <button className="btn btn-accent">Track Progress</button>
