4fca2082dc4954ec466e89c2f68fa9ee3d9953fa
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 20:10:47 -0700

    link logo btn to homepage

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Logo from "@/public/code-braces.svg";
 import User from "@/public/user-smile.svg";
 import Searchbar from "./Searchbar";
@@ -8,10 +9,13 @@ function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl font-extrabold text-neutral">
+        <Link
+          href={"/"}
+          className="btn btn-ghost text-xl font-extrabold text-neutral"
+        >
           <Image src={Logo} alt="Logo" className="h-10 w-10 text-base-200" />
           <span className="hidden md:block">Developers Roadmap</span>
-        </a>
+        </Link>
       </div>
       <div className="flex gap-2">
         {/* Search function */}
