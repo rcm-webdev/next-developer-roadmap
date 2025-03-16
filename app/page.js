079bfc980e80ca121b4a5be1c34226d6b7779aef
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-16 13:09:06 -0700

    add navbar to main page only

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Footer from "@/component/Footer";
 import Hero from "@/component/Hero";
+import Navbar from "@/component/Navbar";
 import Toast from "@/component/Toast";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Hero />
       <Footer />
       <Toast />
