2e0701b8127c40279d7fd81d696178ebec461866
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 20:00:49 -0700

    create button login component

---
+++ component/ButtonLogin.js
@@ -0,0 +1,13 @@
+import { auth } from "../auth";
+import Link from "next/link";
+import Image from "next/image";
+
+function Button() {
+  return (
+    <div>
+      <h1 className="btn">Hello, I am a button</h1>
+    </div>
+  );
+}
+
+export default Button;
