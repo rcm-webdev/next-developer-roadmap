f3152ee26713fd4252db33ce14e385f5b9fd04c5
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 18:56:32 -0700

    update connection with mongodb

--- auth.js
+++ auth.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/libs/mongo";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [],
 });
