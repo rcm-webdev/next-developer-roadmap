cf55a4fc56ff53542d3950b45de928630c92da57
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-16 10:39:26 -0700

    remove session code from the main page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 import Footer from "@/component/Footer";
 import Hero from "@/component/Hero";
 import Toast from "@/component/Toast";
-import { auth } from "@/auth";
 
-export default async function Home() {
-  const session = await auth();
-
+export default function Home() {
   return (
     <div>
       <Hero />
