8f275ef9bd9eaa2a3bc57148d00e0695151b4211
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 21:10:54 -0700

    login/signout and hold session for btn login component

--- component/ButtonLogin.js
+++ component/ButtonLogin.js
@@ -2,11 +2,44 @@ import { auth } from "../auth";
 import Link from "next/link";
 import Image from "next/image";
 
-function Button() {
+async function Button() {
+  const session = await auth();
+  console.log(session);
+
+  const getInitials = (name) => {
+    if (!name) return "NA"; // Default value if name is not available
+    return name
+      .split(" ") // Split the name into parts
+      .slice(0, 2) // Take the first two parts of the name
+      .map((part) => part[0].toUpperCase()) // Get the first letter of each part and capitalize it
+      .join(""); // Join the letters back together
+  };
+
+  if (session && session.user) {
+    const initials = getInitials(session.user.name);
+    return (
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Welcome back - {initials || "Friend"}
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm font-semibold"
+        >
+          <li>
+            <Link href={"/dashboard"}>Dashboard</Link>
+          </li>
+          <li>
+            <Link href={"/api/auth/signout"}>Sign out</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1 className="btn">Hello, I am a button</h1>
-    </div>
+    <Link href="/api/auth/signin" className="btn btn-accent">
+      <span>Login</span>
+    </Link>
   );
 }
 
