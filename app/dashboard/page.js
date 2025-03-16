d713dc69b6d1961777f584d70c571ad1d2233616
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-16 11:03:50 -0700

    add signout btn to dashboard page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,12 @@
+import { SignOut } from "@/component/ButtonSignout";
+
 function Dashboard() {
-  return <div>Hello from the dashboard page</div>;
+  return (
+    <div>
+      <h1>Hello from dashboard page</h1>
+      <SignOut />
+    </div>
+  );
 }
 
 export default Dashboard;
