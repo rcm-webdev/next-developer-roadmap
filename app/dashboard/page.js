d39b4fdc247250e2c978f17392e6ca6f665fb352
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-16 13:08:23 -0700

    add create board comopnent

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,21 @@
 import { SignOut } from "@/component/ButtonSignout";
+import CreateBoard from "@/component/CreateBoard";
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Hello from dashboard page</h1>
-      <SignOut />
-    </div>
+    <main className="bg-base-200 min-h-screen">
+      {/* Header */}
+      <section className="bg-base-100">
+        <div className="px-5 py-5 max-w-5xl mx-auto flex justify-end">
+          <SignOut />
+        </div>
+      </section>
+
+      {/* Create Board */}
+      <section className="max-w-5xl mx-auto px-5 py-12 ">
+        <CreateBoard />
+      </section>
+    </main>
   );
 }
 
