6c136a951a6b2a1de7b797700bffb0db17e9da5d
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 20:01:54 -0700

    add session to be used throughout our application

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import Footer from "@/component/Footer";
 import Hero from "@/component/Hero";
 import Toast from "@/component/Toast";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <Hero />
