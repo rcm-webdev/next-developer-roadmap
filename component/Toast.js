990c1d9b8e556da17dd10f4c33f1cb7bc2319f55
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 17:05:41 -0700

    fix toast width

--- component/Toast.js
+++ component/Toast.js
@@ -1,17 +1,16 @@
 function Toast() {
   return (
     <div>
-      <div className="toast toast-end">
+      <div className="toast toast-end ">
         <div className="alert bg-base-100 rounded-3xl ">
-          <span> 👋🏽 Want to see more projects?</span>
+          <span> 👋🏽 Want to see more projects like this one?</span>
         </div>
-        <div className="btn btn-accent rounded-3xl">
-          <span> ✨ Checkout my github. </span>
+        <div className="btn btn-accent w-fit rounded-3xl">
+          <span> ✨ Checkout my github </span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
             viewBox="0 0 24 24"
+            className="h-6 w-6"
           >
             <path
               fill="none"
