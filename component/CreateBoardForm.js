745d92a1c2f7953dd894f13c008d83021fadedcd
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-16 15:11:22 -0700

    update try/catch error in handleSubmit function

--- component/CreateBoardForm.js
+++ component/CreateBoardForm.js
@@ -1,15 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
 function CreateBoard() {
+  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      await fetch("/api/board", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (err) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <form className="bg-base-100 p-8 rounded-3xl space-y-6">
+    <form
+      onSubmit={handleSubmit}
+      className="bg-base-100 p-8 rounded-3xl space-y-6"
+    >
       {/* Title */}
       <p className="font-bold text-lg">Create a new board</p>
       {/* Form */}
       <fieldset className="fieldset">
         <legend className="fieldset-legend text-lg">Board Name</legend>
-        <input type="text" className="input w-full" placeholder="Type here" />
+        <input
+          required
+          type="text"
+          className="input w-full"
+          placeholder="Future Unicorn Inc 🦄"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </fieldset>
       {/* Button */}
       <button type="submit" className="btn btn-primary btn-block">
+        {isLoading && (
+          <span className="loading loading-spinner loading-md"></span>
+        )}
         Create a new board
       </button>
     </form>
