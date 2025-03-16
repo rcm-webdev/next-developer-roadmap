d78abc2577b8d8c8e897edda52f6403a0731b5aa
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 20:01:10 -0700

    add btn login component on navbar

--- component/Navbar.js
+++ component/Navbar.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Logo from "@/public/code-braces.svg";
 import User from "@/public/user-smile.svg";
 import Searchbar from "./Searchbar";
+import Button from "./ButtonLogin";
 
 function Navbar() {
   return (
@@ -15,34 +16,7 @@ function Navbar() {
       <div className="flex gap-2">
         {/* Search function */}
         <Searchbar />
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <Image src={User} alt="user-avatar" />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-lg dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        <Button />
       </div>
     </div>
   );
