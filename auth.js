902135498df64081689332dd705003a1a316ce16
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-15 19:38:21 -0700

    add discord signin option

--- auth.js
+++ auth.js
@@ -1,8 +1,26 @@
 import NextAuth from "next-auth";
+import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongo";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
-  providers: [],
+  providers: [
+    Resend({
+      // If your environment variable is named differently than default
+      apiKey: process.env.RESEND_KEY,
+      from: "no-reply@resend.rcmcodes.com",
+      name: "Email",
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Discord({
+      clientId: process.env.AUTH_DISCORD_ID,
+      clientSecret: process.env.AUTH_DISCORD_SECRET,
+    }),
+  ],
 });
