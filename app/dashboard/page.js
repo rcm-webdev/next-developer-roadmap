a65b74d4a096e38d38d3d13d3fcfd61c3f295914
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-18 18:56:01 -0700

    design spot to display users created boards

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,19 +1,46 @@
+import ButtonBack from "@/component/ButtonBack";
 import { SignOut } from "@/component/ButtonSignout";
 import CreateBoard from "@/component/CreateBoardForm";
+import { auth } from "@/auth";
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
 
-function Dashboard() {
+async function getUser() {
+  const session = await auth();
+  await connectMongo();
+  return await User.findById(session.user.id).populate("boards");
+}
+
+async function Dashboard() {
+  const user = await getUser();
+
   return (
     <main className="bg-base-200 min-h-screen">
       {/* Header */}
       <section className="bg-base-100">
-        <div className="px-5 py-5 max-w-5xl mx-auto flex justify-end">
+        <div className="px-5 py-5 max-w-5xl mx-auto flex justify-between">
+          <ButtonBack />
           <SignOut />
         </div>
       </section>
 
       {/* Create Board */}
-      <section className="max-w-5xl mx-auto px-5 py-12 ">
+      <section className="max-w-5xl mx-auto px-5 py-12 space-y-12 ">
         <CreateBoard />
+        <div>
+          <h1 className="font-extrabold text-xl mb-4">
+            {user.boards.length} Boards
+          </h1>
+          <ul className="space-y-4">
+            {user.boards.map((board) => {
+              return (
+                <li key={board._id} className="bg-base-100 p-6 rounded-3xl">
+                  {board.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </section>
     </main>
   );
