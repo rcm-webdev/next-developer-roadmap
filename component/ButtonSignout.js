4e33ca85efad642a6678dd682cfad2418f87828e
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-18 18:54:46 -0700

    update sign out to logout

--- component/ButtonSignout.js
+++ component/ButtonSignout.js
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 export function SignOut() {
   return (
     <button onClick={() => signOut()} className="btn btn-ghost">
-      Sign Out
+      Logout
     </button>
   );
 }
