dd8ace1cbbeb211812b067ca9a90545d2ff35e58
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-19 14:59:20 -0700

    add delete board btn component

--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -4,6 +4,7 @@ import Boards from "@/models/Boards";
 import { auth } from "@/auth";
 import ButtonBack from "@/component/ButtonBack";
 import CardBoardLink from "@/component/CardBoardLink";
+import ButtonDeleteBoard from "@/component/ButtonDeleteBoard";
 
 const getBoard = async (boardId) => {
   const session = await auth();
@@ -41,6 +42,7 @@ async function FeebackBoard({ params }) {
       <section className="max-w-5xl mx-auto px-5 py-12 space-y-12 ">
         <h1 className="font-extrabold text-xl mb-4">{board.name}</h1>
         <CardBoardLink boardId={board._id} />
+        <ButtonDeleteBoard boardId={board._id} />
       </section>
     </main>
   );
