fe2ad76d8acc6fef8ef1b3f1027d8f4a843b612f
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-19 11:33:44 -0700

    access only to users who created boards can update them

--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -1,7 +1,30 @@
+import { redirect } from "next/navigation";
+import connectMongo from "@/libs/mongoose";
+import Boards from "@/models/Boards";
+import { auth } from "@/auth";
+
+const getBoard = async (boardId) => {
+  const session = await auth();
+
+  await connectMongo();
+  const board = await Boards.findOne({
+    _id: boardId,
+    userId: session?.user?.id,
+  });
+
+  if (!board) {
+    redirect("/dashboard");
+  }
+
+  return board;
+};
+
 async function FeebackBoard({ params }) {
   const { boardId } = await params;
 
-  return <div>{boardId}</div>;
+  const board = await getBoard(boardId);
+
+  return <div>{board.name}</div>;
 }
 
 export default FeebackBoard;
