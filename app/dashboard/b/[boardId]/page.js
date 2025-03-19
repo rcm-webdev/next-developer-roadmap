2c83b95a125385d433103f5a56f7b1f7be8382d7
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-19 11:59:13 -0700

    style private feedback board page and added the back btn component to get back to dashboard

--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import connectMongo from "@/libs/mongoose";
 import Boards from "@/models/Boards";
 import { auth } from "@/auth";
+import ButtonBack from "@/component/ButtonBack";
 
 const getBoard = async (boardId) => {
   const session = await auth();
@@ -24,7 +25,19 @@ async function FeebackBoard({ params }) {
 
   const board = await getBoard(boardId);
 
-  return <div>{board.name}</div>;
+  return (
+    <main className="bg-base-200 min-h-screen">
+      {/* Header */}
+      <section className="bg-base-100">
+        <div className="px-5 py-5 max-w-5xl mx-auto flex justify-start">
+          <ButtonBack />
+        </div>
+      </section>
+      <section className="max-w-5xl mx-auto px-5 py-12 space-y-12 ">
+        <h1 className="font-extrabold text-xl mb-4">{board.name}</h1>
+      </section>
+    </main>
+  );
 }
 
 export default FeebackBoard;
